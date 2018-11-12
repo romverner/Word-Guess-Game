09e56e286be6f8f1d3d55407d702e5d655ec3d49
Roman Verner <romverj@gmail.com>
2018-11-12 18:01:39 -0500

    Check if user input valid

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,10 +8,18 @@ document.onkeyup = function(event) {
     // Setting local variables
     var userGuess = event.key;
 
-    // Tracks user keys, determines if letter has been used.
+    // Tracks user keys, determines if letter has been used or is an invalid input.
     console.log(userGuess);
     if (letterArray.includes(userGuess) === true && guessedLetters.includes(userGuess) === false) {
         guessedLetters.push(String(userGuess));
         console.log("You have guessed a new letter.");
     }
+
+    else if (guessedLetters.includes(userGuess) === true) {
+        console.log("You've already guessed that letter!");
+    }
+
+    else {
+        console.log("Please enter a valid letter!");
+    };
 };
