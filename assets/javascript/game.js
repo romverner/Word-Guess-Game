08b6488509652a04ae5339fc594642be044ac998
Roman Verner <romverj@gmail.com>
2018-11-14 11:21:32 -0500

    Building up the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,34 +3,44 @@ var wordArray = ['amazing', 'jazz', 'automobile', 'hamster'];
 var letterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var guessedLetters = [];
 var randomWord = "";
+var parsedWord = [];
+var hiddenWord = [];
 
-// Function that randomly chooses a word from wordArray
-function randWord() {
-    var localWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-    return localWord;
-};
-
-// Sets the first word
-randomWord = randWord();
-
-// Function that runs when it detects if key is pressed
+// Function that runs when it detects if key is pressed, controls entire game
 document.onkeyup = function(event) {
 
-    // Setting local variables
+    // Storing key presses
     var userGuess = event.key;
 
-    // Parsing out letters from randomWord and creating array
-    var parsedWord = randomWord.split("");
-    var onScreenWord = parsedWord.slice();
-    
-    // Console logs for troubleshooting
-    console.log(parsedWord);
-    console.log(userGuess);
-    console.log(randomWord);
-    console.log(onScreenWord);
+    // Function that randomly chooses a word from wordArray
+    function randWord() {
+        var localWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+        return localWord;
+    };
+
+    // Function that parses the letters from randomWord
+    function parseWord(word) {
+        var localWord = word.split("");
+        return localWord;
+    }
+
+    // Creating a hidden version of chosen word
+    function underWord() {
+        var localWord = [];
+        while (parsedWord.length > localWord.length) {
+            localWord.push("_");
+        }
+        return localWord;
+    };
 
     // Tracks user keys, determines if letter has been used or is an invalid input
-    if (letterArray.includes(userGuess) === true && guessedLetters.includes(userGuess) === false && parsedWord.includes(userGuess) === true) {
+    if (userGuess === '=') {
+        randomWord = randWord();
+        parsedWord = parseWord(randomWord);
+        hiddenWord = underWord();
+    }
+
+    else if (letterArray.includes(userGuess) === true && guessedLetters.includes(userGuess) === false && parsedWord.includes(userGuess) === true) {
         guessedLetters.push(String(userGuess));
         console.log("Correct!");
     }
@@ -43,11 +53,12 @@ document.onkeyup = function(event) {
         console.log("You've already guessed that letter!");
     }
 
-    else if (userGuess === '=') {
-        randomWord = randWord();
-    }
-
     else {
         console.log("Please enter a valid letter!");
     };
+
+    // Console.logs for troubleshooting
+    console.log(randomWord);
+    console.log(parsedWord);
+    console.log(hiddenWord);
 };
