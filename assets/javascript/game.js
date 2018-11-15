711a4f110fa287baac9a6fd895bd4531eaf406f2
Roman Verner <romverj@gmail.com>
2018-11-15 10:36:00 -0500

    Added winning condition

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // Setting global variables
-var wordArray = ['amazing', 'jazz', 'automobile', 'hamster', 'patrick'];
+var wordArray = ['richard', 'jeremy', 'james', 'power', 'hammer', 'koenigsegg', 'oliver'];
 var letterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var guessedLetters = [];
 var userGuessCount = 0;
@@ -39,41 +39,82 @@ document.onkeyup = function(event) {
         return localWord;
     };
 
-    // Tracks user keys, determines if letter has been used or is an invalid input
-    if (userGuess === '=') {
+    // Checks for winning condition
+    function winningCondition(array) {
+        var numCount = 0;
+        for (i = 0; i < parsedWord.length; i++) {
+            if (letterArray.includes(array[i]) === true) {
+                numCount += 1;
+                if (numCount === parsedWord.length) {
+                    return true;
+                };
+            };
+        };
+    };
+
+    // If game not over, tracks user keys, determines if letter has been used or is an invalid input
+    if (userGuessCount < 7) {
+        if (userGuess === '=') {
+            randomWord = randWord();
+            parsedWord = parseWord(randomWord);
+            hiddenWord = underWord();
+            hiddenLettersDoc.textContent = hiddenWord;
+            guessedLetters = [];
+            guessedLettersDoc.textContent = guessedLetters;
+            userInfoDoc.textContent = "Guess the word that appears here!";
+        }
+
+        else if (winningCondition(hiddenWord) === true) {
+            userInfoDoc.textContent = "You Win! Press '=' to try a new word.";
+        }
+
+        else if (letterArray.includes(userGuess) === true && guessedLetters.includes(userGuess) === false && parsedWord.includes(userGuess) === true) {
+            guessedLetters.push(String(userGuess));
+            guessedLettersDoc.textContent = guessedLetters;
+            userInfoDoc.textContent = "Correct!";
+
+            // For loop that reveals letters in hiddenWord if correctly guessed
+            for (i = 0; i < hiddenWord.length; i++) {
+                var location = parsedWord.indexOf(userGuess, i);
+                hiddenWord[location] = userGuess;
+                hiddenLettersDoc.textContent = hiddenWord;
+            };
+
+            if (winningCondition(hiddenWord) === true) {
+                userInfoDoc.textContent = "You Win! Press '=' to try a new word.";
+            }
+        }
+
+        else if (letterArray.includes(userGuess) === true && guessedLetters.includes(userGuess) === false && parsedWord.includes(userGuess) === false) {
+            guessedLetters.push(String(userGuess));
+            guessedLettersDoc.textContent = guessedLetters;
+            userGuessCount += 1;
+            
+            // Checking number of incorrect user attempts, game over if seven attempts
+            if (userGuessCount === 1) {
+                userInfoDoc.textContent = "Game Over!";
+            }
+            userInfoDoc.textContent = "Incorrect!";
+        }
+
+        else if (guessedLetters.includes(userGuess) === true) {
+            guessedLettersDoc.textContent = guessedLetters;
+            userInfoDoc.textContent = "You've already guessed that letter!";
+        }
+
+        else {
+            userInfoDoc.textContent = "Please enter a valid letter!";
+        };
+    }
+
+    else {
+        userInfoDoc.textContent = ("Game Over! The word was: " + randomWord + ". Try this new word!");
         randomWord = randWord();
         parsedWord = parseWord(randomWord);
         hiddenWord = underWord();
         hiddenLettersDoc.textContent = hiddenWord;
         guessedLetters = [];
         guessedLettersDoc.textContent = guessedLetters;
-    }
-
-    else if (letterArray.includes(userGuess) === true && guessedLetters.includes(userGuess) === false && parsedWord.includes(userGuess) === true) {
-        guessedLetters.push(String(userGuess));
-        guessedLettersDoc.textContent = guessedLetters;
-        userInfoDoc.textContent = "Correct!";
-
-        // For loop that reveals letters in hiddenWord if correctly guessed
-        for (i = 0; i < hiddenWord.length; i++) {
-            var location = parsedWord.indexOf(userGuess, i);
-            hiddenWord[location] = userGuess;
-            hiddenLettersDoc.textContent = hiddenWord;
-        };
-    }
-
-    else if (letterArray.includes(userGuess) === true && guessedLetters.includes(userGuess) === false && parsedWord.includes(userGuess) === false) {
-        guessedLetters.push(String(userGuess));
-        guessedLettersDoc.textContent = guessedLetters;
-        userInfoDoc.textContent = "Incorrect!";
-    }
-
-    else if (guessedLetters.includes(userGuess) === true) {
-        guessedLettersDoc.textContent = guessedLetters;
-        userInfoDoc.textContent = "You've already guessed that letter!";
-    }
-
-    else {
-        userInfoDoc.textContent = "Please enter a valid letter!";
+        userGuessCount = 0;
     };
 };
